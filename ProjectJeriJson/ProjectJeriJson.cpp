a2010134c8ed1ebfe4c5c7919e254ebe26decca7
Jerilee <rongerace@163.com>
2021-02-02 15:55:37 +0800

    新建项目

---
+++ ProjectJeriJson/ProjectJeriJson.cpp
@@ -0,0 +1,16 @@
+﻿// ProjectJeriJson.cpp: 定义应用程序的入口点。
+//
+
+#include "ProjectJeriJson.h"
+#include "JeriJson/JeriJson.h"
+
+using namespace std;
+
+int main()
+{
+	int x = 0;
+	TestA::Tee(x);
+	cout << x << endl;
+	cout << "Hello CMake." << endl;
+	return 0;
+}
